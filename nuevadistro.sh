418c8e7ae671a95661018ab4f94a45efeffd32bf
Isaías Gätjens M <66392244+igatjens@users.noreply.github.com>
2020-09-09 13:59:30 -0600

    Add files via upload

---
+++ nuevadistro.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DISTRO=apricot
+MIRROR=http://packages.deepin.com/deepin/dists/
+DIR_TRABAJO=~/
+
+echo $DIR_TRABAJO
+
+cd $DIR_TRABAJO
+
+if [[ $(curl $MIRROR 2> /dev/null | grep "$DISTRO") ]]; then
+	#statements
+	echo $(date) - Ya agregaron $DISTRO a $MIRROR >> nuevadistro.txt
+	export DISPLAY=:0
+	xdg-open nuevadistro.txt &
+	xdg-open https://osdn.net/projects/deepin/storage/
+	xdg-open http://packages.deepin.com/deepin-cd/
+fi
